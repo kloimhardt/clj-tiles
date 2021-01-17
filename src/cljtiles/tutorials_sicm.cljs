d3e79aed43fd1d519ba436e623dd0d37d8ceb989
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2021-01-17 01:46:18 +0100

    continue tutorial

--- src/cljtiles/tutorials_sicm.cljs
+++ src/cljtiles/tutorials_sicm.cljs
@@ -10,29 +10,7 @@
 (def chapnames ["SICM"])
 
 (def e-vect
-  ["Farming"
-   [0 0]
-   (gb/rpg [[0 0] [150 0] [300 0]
-            [0 50] [300 50]
-            [0 100] [300 100]
-            [0 150] [300 250] [400 250] [550 250]
-            [0 300]]
-           ''sheep
-           '(+ :tiles/slot :tiles/slot)
-           ''sheep
-           ''fish
-           '(* 'ca20 :tiles/slot)
-           '(array-map :tiles/slot :tiles/slot
-                       :tiles/slot :tiles/slot)
-           '2
-           '(defn count-animals [[breed quantity]]
-              :tiles/slot)
-           'quantity
-           '(* :tiles/slot :tiles/slot)
-           'breed
-           '(map count-animals :tiles/slot)
-           )
-   [:div
+  [[:div
     [:p "We start by creating a function Path-of-a-Free-Particle. Newtons first law states that in some inertial frame of reference, an object continues to move in space at a constant velocity. This movement takes time, so our function depends on time. It returns a vector of two elements because we choose our path to live in two dimensions."]
     [:p "
 In familiar notation, the path is denoted by (note that \\(\\vec{x}\\) is a column vector with superscripted component-indizes, hence the name \"up\" in the code):
@@ -65,9 +43,8 @@ It is a moving body which is at time \\(t=0\\) at point
 \\end{smallmatrix}\\big)\\)
 and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \\frac{m}{s}\\) in \\(y\\) direction. Imagine the body as a "
      [:a {:href "https://www.youtube.com/watch?v=z74OwRy8o9I"} "Pizza in space"]
-     " (and always think of a sattelite when someone talks about \"inertial frame of reference\")"]
+     " (and always think of a sattelite when someone talks about \"inertial frame of reference\")"]]
 
-    ]
    [0 0]
    (gb/rpg [[0 0]
             [0 90]
@@ -92,16 +69,16 @@ and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \
    "Run the program. The output does not convey much yet"
    [0 0]
    (gb/rpg [[0 0]]
-     '(defn Path-of-a-Free-Particle
-       time
-       (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time))))))
+           '(defn Path-of-a-Free-Particle
+              time
+              (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time))))))
    [:div
     [:p "The kinetic energy is half the mass times velocity squared. If we set the mass to \\(2kg\\) the formula becomes simpler. We know that the velocity of our free particle is constant. In the \\(x\\) direction it is \\(5 \\frac{m}{s}\\). But notice that the velocity here could be any function in time (not in space+time.)"]
     [:p "You define this kinetic enery as yet another function. It is not yet connected to the one defined before. The kinetic energy is a function of velocity, \\(T=T(\\vec{v}) = \\frac{m}{2} |\\vec{v}|^2 = \\frac{m}{2}v^2\\). With \\(m=2\\) you get \\( T=v^2\\) and with \\(v=\\sqrt{5^2 + 4^2}\\frac{m}{s}\\), you get \\(T=41 \\frac{kg\\ m^2}{s^2}\\). So, \\(v\\) is a constant here, but will become a function of time \\(v=v(t)\\). That means you need to accept the fact that \\(T\\) will become a function of a function. And, to make no mistake, velocity never is a function of position but only of time."]]
    [0 0]
    (gb/rpg [[0 0] [0 170] [0 260] [0 300] [0 340] [150 340]]
            '(defn Path-of-a-Free-Particle
-             time
+              time
               (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time)))))
            '(defn :tiles/slot :tiles/slot :tiles/slot)
            'Kinetic-Energy
@@ -112,8 +89,8 @@ and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \
    [0 0]
    (gb/rpg [[0 0] [0 170]]
            '(defn Path-of-a-Free-Particle
-             time
-             (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time)))))
+              time
+              (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time)))))
            '(defn Kinetic-Energy
               velocity
               (square velocity)))
@@ -125,8 +102,8 @@ and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \
    (gb/rpg [[0 0] [0 170] [0 280] [150 280] [150 330] [350 330]
             [300 250] [400 250] [550 250] [650 250] [750 250]]
            '(defn Path-of-a-Free-Particle
-             time
-             (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time)))))
+              time
+              (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time)))))
            '(defn Kinetic-Energy
               velocity
               (square velocity))
@@ -143,14 +120,14 @@ and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \
    [0 0]
    (gb/rpg [[0 0] [0 170] [0 280]]
            '(defn Path-of-a-Free-Particle
-             time
+              time
               (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time)))))
            '(defn Kinetic-Energy
-             velocity
-             (square velocity))
+              velocity
+              (square velocity))
            '(defn Lagrangian
-             [[time position velocity]]
-             (Kinetic-Energy velocity)))
+              [[time position velocity]]
+              (Kinetic-Energy velocity)))
    [:div
     [:p "The lagrangian equations are differential equations. They are constructed out of the Lagrangian function. The differential equations are applied to some path function. Here we use our specific Path-of-a-Free-Particle. The result is in general a function of time. We set the time to ten seconds here. If the path we fed into the equtions is a viable physical path, the result shoud be a very special function: it should be the zero vector."]]
    [0 -170]
@@ -173,8 +150,7 @@ and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \
            '(Lagrange-equations :tiles/slot)
            'Lagrangian
            'Path-of-a-Free-Particle
-           '10
-           )
+           '10)
    "Print the result and find out whether you indeed see the zero vector."
    [0 -170]
    (gb/rpg [[0 0] [0 170] [0 280]
@@ -189,10 +165,9 @@ and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \
               [[time position velocity]]
               (Kinetic-Energy velocity))
            '(tex "Title"
-                (((Lagrange-equations Lagrangian)
-                  Path-of-a-Free-Particle)
-                 10))
-           )
+                 (((Lagrange-equations Lagrangian)
+                   Path-of-a-Free-Particle)
+                  10)))
    "You replace the Path-of-a-Free-Particle. You create a vector of two arbitrary funtions. Call them q_x and q_y. Then do the replacement of the specific path with the arbitrary path. After running the workspace again, you see the general equations of motion for the free particle."
    [0 -200]
    (gb/rpg [[0 0] [0 170] [0 280] [0 420]
@@ -214,8 +189,7 @@ and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \
            '(literal-function :tiles/slot)
            'q_x
            '(literal-function :tiles/slot)
-           'q_y
-           )
+           'q_y)
    "Run the workspace to see the general equations of motion for the free particle."
    [0 -200]
    (gb/rpg [[0 0] [0 170] [0 280] [0 420]]
@@ -229,13 +203,111 @@ and has a constant speed of \\(5 \\frac{m}{s}\\) in \\(x\\) direction and \\(4 \
               [[time position velocity]]
               (Kinetic-Energy velocity))
            '(tex "Title"
-                (((Lagrange-equations Lagrangian)
-                  (up (literal-function 'q_x)
-                      (literal-function 'q_y)))
-                 10))
-           )
+                 (((Lagrange-equations Lagrangian)
+                   (up (literal-function 'q_x)
+                       (literal-function 'q_y)))
+                  10)))
+   "Now, you want to make the equations prettier. First, you replace 10 by some time \\(t\\),
+second you introduce the mass \\(m\\) into the kinetic energy. You throw away the specific path, it is not needed anymore."
+   [0 -200]
+   (gb/rpg [[0 0] [0 170] [0 280] [0 420]
+            [0 550] [150 550] [300 550] [350 550] [700 550]]
+           '(defn Path-of-a-Free-Particle
+              time
+              (:tiles/vert (up (+ 2 (* 5 time)) (+ 3 (* 4 time)))))
+           '(defn Kinetic-Energy
+              velocity
+              (square velocity))
+           '(defn Lagrangian
+              [[time position velocity]]
+              (Kinetic-Energy velocity))
+           '(tex "Title"
+                 (((Lagrange-equations Lagrangian)
+                   (up (literal-function 'q_x)
+                       (literal-function 'q_y)))
+                  10))
 
-  ])
+           '(* :tiles/slot :tiles/slot)
+           '(/ :tiles/slot :tiles/slot)
+           ''m
+           '2
+           ''t)
+   "After running, you see Newtons equations of motion for the two dimensional free particle in their standard form."
+   [0 0]
+   (gb/rpg [[0 0] [0 150] [0 300]]
+           '(defn Kinetic-Energy
+              velocity
+              (* (/ 'm 2) (square velocity)))
+           '(defn Lagrangian
+              [[time position velocity]]
+              (Kinetic-Energy velocity))
+           '(tex "Title"
+                 (((Lagrange-equations Lagrangian)
+                   (up (literal-function 'q_x)
+                       (literal-function 'q_y)))
+                  't)))
+   "Now we model a particle in the gravitational field by introducing the potential energy. It depends on
+the hight of the particle above ground. It is \\(m \\times g \\times hight\\), where \\(g\\) is \\(9.81 \\frac{m}{s^2}\\), the acceleration due to the gravity of the earth. The Lagrangain is kintic energy minus potential energy. And the Lagrangian now depends on the hight."
+   [0 0]
+   (gb/rpg [[0 0] [0 150]
+            [100 120] [100 170]
+            [100 220] [180 220] [300 220] [380 220] [500 220]
+            [0 300]
+            [450 300] [550 300] [600 300]
+            [300 400] [450 400] [650 400]
+            [0 450]]
+           '(defn Kinetic-Energy
+              velocity
+              (* (/ 'm 2) (square velocity)))
+           '(defn :tiles/slot
+              :tiles/slot
+              :tiles/slot)
+           'Potential-Energy
+           'hight
+           ''m
+           '(* :tiles/slot :tiles/slot)
+           ''g
+           '(* :tiles/slot :tiles/slot)
+           'hight
+
+           '(defn Lagrangian
+              [[time position velocity]]
+              (Kinetic-Energy velocity))
+           [:tiles/slot :tiles/slot]
+           '_
+           'hight
+           '(- :tiles/slot :tiles/slot)
+           '(Potential-Energy :tiles/slot)
+           'hight
+
+           '(tex "Title"
+                 (((Lagrange-equations Lagrangian)
+                   (up (literal-function 'q_x)
+                       (literal-function 'q_y)))
+                  't)))
+   "Run the workspace and Newtons equations of motion for the particle in the homogeneous gravitational field
+are delivered."
+   [0 0]
+   (gb/rpg [[0 0]
+            [0 150]
+            [0 300]
+            [0 450]]
+           '(defn Kinetic-Energy
+              velocity
+              (* (/ 'm 2) (square velocity)))
+           '(defn Potential-Energy
+              hight
+              (* (* 'm 'g) hight))
+           '(defn Lagrangian
+              [[time [_ hight] velocity]]
+              (- (Kinetic-Energy velocity)
+                 (Potential-Energy hight)))
+           '(tex "Title"
+                 (((Lagrange-equations Lagrangian)
+                   (up (literal-function 'q_x)
+                       (literal-function 'q_y)))
+                  't)))
+   ])
 
 (defn count-animals [quantity breed]
   (if ((hash-set 'fish 'sheep) breed)
