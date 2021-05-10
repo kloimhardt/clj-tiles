a594d7dfbdf955340c85333d0ffe8d2ed065f231
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2021-05-11 00:26:32 +0200

    more lyrics tutorials

--- src/cljtiles/tutorials_achangin.cljs
+++ src/cljtiles/tutorials_achangin.cljs
@@ -1,15 +1,120 @@
 (ns cljtiles.tutorials-achangin)
 
+(defn lyric-format [textvec hash-code]
+  (println "thehash " (hash textvec))
+  (when (= (hash textvec) hash-code)
+    (fn [] (into [:<>] (map #(vector :p %) textvec)))))
+
+(defn lyric-from-vec [textvec hash-code]
+  (println "thehash " (hash textvec))
+  (when (= (hash textvec) hash-code)
+    (fn [] (into [:<>] (map #(vector :p (apply str (interpose " " %)))
+                            textvec)))))
+
 (def content
   {:chapnames ["Lyrics"]
    :chaps [2] #_(count (:tutorials content))
    :tutorials
    [{:code ["Come gather 'round people"]
      :comp-fn (fn [{:keys [result-raw]}]
-                (when (= (hash result-raw) 1745298409)
-                  (fn [] [:p result-raw])))}
-    {:code ['(:tiles/vert (printlns "Come gather 'round people"
-                                    "Wherever you roam"))]
+                (lyric-format [result-raw] -472680540))}
+    {:code ['(:tiles/vert (printlns "Wherever you roam"
+                                    "And admit that the waters"))]
      :comp-fn (fn [{:keys [stdout]}]
-                (when (= (hash stdout) 1058487531)
-                  (fn [] (into [:<>] (map #(vector :p %) stdout)))))}]})
+                (lyric-format stdout 1698792518))}
+    {:blockpos [[0 0] [0 100] [0 150]]
+     :code ['(:tiles/vert (printlns :tiles/slot
+                                    "And accept it that soon"
+                                    :tiles/slot))
+            "Around you have grown"
+            "You'll be drenched to the bone"]
+     :comp-fn (fn [{:keys [stdout]}]
+                (lyric-format stdout -1282138944))}
+    {:blockpos [[0 0] [0 150] [0 200] [0 250]]
+     :code ['(:tiles/vert (printlns "If your time to you"
+                                    :tiles/slot
+                                    "Then you better start swimmin'"
+                                    :tiles/slot
+                                    ))
+            "For the times they are a-changin'"
+            "Is worth savin'"
+            "Or you'll sink like a stone"
+            ]
+     :comp-fn (fn [{:keys [stdout result-raw]}]
+                (lyric-format (conj stdout result-raw) 1352137510))}
+    {:blockpos [[0 0] [0   200] [0   250] [0   300]
+                      [300 200] [300 250] [300 300]]
+     :code ['(:tiles/vert (printlns :tiles/slot
+                                    :tiles/slot
+                                    :tiles/slot
+                                    :tiles/slot
+                                    :tiles/slot
+                                    :tiles/slot))
+            "You'll be drenched to the bone"
+            "Wherever you roam"
+            "Around you have grown"
+            "And admit that the waters"
+            "And accept it that soon"
+            "Come gather 'round people"]
+     :comp-fn (fn [{:keys [stdout]}]
+                (lyric-format stdout 331859347))}
+    {:blockpos [[0 0] [0   200] [0   250] [0   300]
+                [300 200] [300 250] [300 300]]
+     :code ['(:tiles/vert (printlns :tiles/slot
+                                    :tiles/slot
+                                    :tiles/slot
+                                    :tiles/slot))
+            "Is worth savin'"
+            "For the times they are a-changin'"
+            "Or you'll sink like a stone"
+            "If your time to you"
+            "Then you better start swimmin'"]
+     :comp-fn (fn [{:keys [stdout result-raw]}]
+                (lyric-format (conj stdout result-raw) 1352137510))}
+    {:blockpos [[0 0] [0 50] [0 100]]
+     :code ['(str :tiles/slot :tiles/slot)
+            "For the wheel's still in sp"
+            "in"]
+     :comp-fn (fn [{:keys [result-raw]}]
+                (lyric-format [result-raw] -1697619396))}
+    {:blockpos [[0 0] [0 50] [0 100] [0 150] [0 200] [0 250]]
+     :code ['(str :tiles/slot :tiles/slot :tiles/slot :tiles/slot)
+            "For the times they are"
+            " "
+            "a-chang"
+            "in'"]
+     :comp-fn (fn [{:keys [result-raw]}]
+                (lyric-format [result-raw] -1399556977))}
+    {:blockpos [[0 0]
+                [0 300] [100 300] [200 300] [300 300] [400 300] [500 300]]
+     :code ['(:tiles/vert
+              [["Come" :tiles/slot "and critics"]
+               [:tiles/slot "prophesize with your pen"]
+               ["And keep your" :tiles/slot "wide"]
+               ["The chance" :tiles/slot "come again"]
+               ["And don’t" :tiles/slot "too soon"]
+               ["For the wheel’s still in" :tiles/slot]])
+            ''writers
+            ''Who
+            ''spin
+            ''eyes
+            ''won't
+            ''speak
+            ]
+     :comp-fn (fn [{:keys [result-raw]}]
+                (lyric-from-vec result-raw -395749415))}
+    {:blockpos [[0 0]
+                [0 300] [100 300] [200 300] [300 300] [400 300] [500 300]]
+     :code ['(:tiles/vert
+              [[:tiles/slot "there’s no tellin’ who"]
+               ["that it’s" :tiles/slot]
+               ["For the loser" :tiles/slot]
+               ["will be" :tiles/slot "to win"]
+               ["For the" :tiles/slot "they are a-changin’"]])
+            ''namin’
+            ''now
+            ''And
+            ''later
+            ''times]
+     :comp-fn (fn [{:keys [result-raw]}]
+                (lyric-from-vec result-raw 792318763))}]})
